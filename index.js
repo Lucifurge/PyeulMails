eda950a76262c294f7584576fcde1f8db1073928
Lucifurge <tasogaresaya@gmail.com>
2025-02-01 21:36:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const lockScreen = () => {
         const credentials = [
             { username: "mariz", password: "mariz2006" },
-            { username: "realwifenimingyu", password: "arousseau_18" },
             { username: "lucifurge", password: "09100909" },
-            { username: "kupikupin", password: "mwamwachopchop_lezi" },
-            { username: "@krizjorjas", password: "krizzy123" },
-            { username: "@gyo.cute", password: "hrldsynth" },
+             { username: "asherfinn", password: "asher00" },
             ...Array(35).fill({ username: "", password: "" })
         ];
 
